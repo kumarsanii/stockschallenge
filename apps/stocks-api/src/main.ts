373e2596a0ebcaf3c2ea1f3880fbd9d37bdb1319
sandkr <sandkr496@gmail.com>
2020-02-23 20:07:39 -0800

    adding api timeout.

--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -2,12 +2,50 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  **/
+const _ = require('lodash');
 import { Server } from 'hapi';
+const config = require('./config.json');
+const defaultConfig = config.development;
+const environment = process.env.NODE_ENV || 'development';
+const environmentConfig = config[environment];
+const finalConfig = _.merge(defaultConfig, environmentConfig);
 
+console.log(finalConfig);
 const init = async () => {
-  const server = new Server({
-    port: 3333,
-    host: 'localhost'
+  // const server = new Server({
+  //   port: finalConfig.port,
+  //   host: finalConfig.host
+  // });
+
+  var server = new Server();
+  server.connection({ port: 80 });
+  var remotes = {
+    url: finalConfig.host + ':' + finalConfig.port,
+    path: '/'
+  };
+
+  server.route({
+    method: '*',
+    path: remotes.path,
+    handler: {
+      proxy: {
+        mapUri: function(request, callback) {
+          var url =
+            remotes.url +
+            '/' +
+            request.url.href.replace('/' + remotes.path + '/', '');
+          callback(null, url);
+        },
+        passThrough: true,
+        xforward: true
+      }
+    },
+    options: {
+      cache: {
+        expiresIn: 30 * 1000, //expiresIn to 30 seconds
+        privacy: 'private'
+      }
+    }
   });
 
   server.route({
